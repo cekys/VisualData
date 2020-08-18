2bf5e89c463eb3b0c32d8309c0cdb670ccb4eaac
cekys <10346089+cekys@users.noreply.github.com>
2020-08-18 17:46:59 +0800

    update

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-let mock = require('../mock/index')
+let mock = require('../DataMock/index')
+let express = require('express')
 
-let template = {
-    'list|1-10': [{
-        'id|+1': 1,
-        'name': '@name'
+// express 服务器托管web文件
+let app = express()
+app.use('/web', express.static('public'))
+let server = app.listen(80, function () {
+    console.log('web server visit: http://localhost:' + server.address().port + '/web')
+})
+
+// 仿真器服务器
+let template = [
+    {
+        'gender': 'female',
+        'count|1-1000': 1
+    },
+    {
+        'gender': 'male',
+        'count|1-1000': 1
     }]
-}
-
-mock.server('/mock', template)
+mock.startServer('/mock/gender', template)
